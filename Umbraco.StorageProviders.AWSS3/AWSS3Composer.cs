977a08805bff5c687187cd2325081f06824f9216
Adam Werner <adam.werner@castellantech.com>
2022-09-19 12:10:17 -0400

    Added composer for AWSS3
    - using Amazon.Extensions.NETCore.Setup method to get default AWS options
    - using Amazon.Extensions.NETCore.Setup method to establish IAmazonS3 (S3 client) available with dependency injection

---
+++ Umbraco.StorageProviders.AWSS3/AWSS3Composer.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Extensions.NETCore.Setup;
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Umbraco.StorageProviders.AWSS3
+{
+    public class AWSS3Composer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            AWSOptions awsOptions = builder.Config.GetAWSOptions();
+
+            builder.Services.AddDefaultAWSOptions(awsOptions);
+            builder.Services.AddAWSService<IAmazonS3>();
+        }
+    }
+}
